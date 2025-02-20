60afdfd9ec0f5c3015f17df6b5090774e9fa2d22
Coralia Manzanilla Pech <coralia.manzanillapech@wur.nl>
2025-02-20 10:57:06 +0100

    bash2.sh

--- bash2.sh
+++ bash2.sh
@@ -2,7 +2,7 @@
 
 ml use /lustre/shared/Courses/RELIVESTOCK2025/modules
 #rm /home/WUR/manza003/.asreml/asreml_active
-rm /home/WUR/manza003/VSNi
+#rm /home/WUR/manza003/VSNi
 module load asreml/4.2.1
 
 time asreml -n uni.as
